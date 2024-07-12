58191738cae1f0afcd806b7167dde64497f83b96
Chin Leung <hello@chinleung.com>
2024-07-12 11:58:50 -0400

    Remove lazy load fixing the highlights not loading properly

--- vim/lua/plugins/git.lua
+++ vim/lua/plugins/git.lua
@@ -1,5 +1,6 @@
 return {
     'tpope/vim-fugitive',
+    lazy = false,
     config = function ()
         vim.cmd('hi DiffAdd guibg=#282828 ctermbg=235 guifg=#8ec07c ctermfg=142 cterm=NONE gui=NONE')
         vim.cmd('hi DiffChange guibg=#282828 ctermbg=235 guifg=#fdba48 ctermfg=108 cterm=NONE gui=NONE')
