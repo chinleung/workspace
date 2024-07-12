7c1c4b43a9ba22a85c7eecbc17d863dffccc536c
Chin Leung <hello@chinleung.com>
2024-07-12 18:39:11 -0400

    Fixed airline seperators

--- vim/lua/plugins/airline.lua
+++ vim/lua/plugins/airline.lua
@@ -1,7 +1,12 @@
 return {
-    'bling/vim-airline',
+    'vim-airline/vim-airline',
+    lazy = false,
     config = function ()
+        vim.g.airline_left_alt_sep = ''
+        vim.g.airline_left_sep = ''
         vim.g.airline_powerline_fonts = 1
+        vim.g.airline_right_alt_sep = ''
+        vim.g.airline_right_sep = ''
         vim.g['airline#extensions#tabline#enabled'] = 1
         vim.g['airline#extensions#tabline#fnamemod'] = ':t'
     end,
