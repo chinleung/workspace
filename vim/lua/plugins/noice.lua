96fe34f610aa3d53aab9ca6d2fcf0edf91fd4713
Chin Leung <hello@chinleung.com>
2024-07-16 22:36:37 -0400

    Fixed the styling of different inputs

--- vim/lua/plugins/noice.lua
+++ vim/lua/plugins/noice.lua
@@ -23,9 +23,27 @@ return {
                 input = {
                     view = 'cmdline_input',
                     icon = '󰥻 ',
+                    icon_hl_group = 'GruvboxBlue',
+                    opts = {
+                        win_options = {
+                            winhighlight = {
+                                FloatBorder = 'GruvboxBlue',
+                                FloatTitle = 'GruvboxBlue',
+                            }
+                        },
+                    }
                 },
                 search_down = {
                     icon = ' ',
+                    icon_hl_group = 'GruvboxYellow',
+                    opts = {
+                        win_options = {
+                            winhighlight = {
+                                FloatBorder = 'GruvboxYellow',
+                                FloatTitle = 'GruvboxYellow',
+                            }
+                        },
+                    }
                 },
                 laravel = {
                     view = 'cmdline_input',
@@ -44,7 +62,16 @@ return {
                 terminal = {
                     view = 'cmdline_input',
                     icon = ' ',
+                    icon_hl_group = 'GruvboxAqua',
                     pattern = '^:split | terminal',
+                    opts = {
+                        win_options = {
+                            winhighlight = {
+                                FloatBorder = 'GruvboxAqua',
+                                FloatTitle = 'GruvboxAqua',
+                            }
+                        },
+                    },
                 }
             },
         },
