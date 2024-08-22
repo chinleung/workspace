42af8d04ee34e38defbf1d291d2c66cc5eecfa58
Chin Leung <hello@chinleung.com>
2024-08-22 11:53:47 -0400

    Added silent

--- vim/lua/plugins/git.lua
+++ vim/lua/plugins/git.lua
@@ -7,12 +7,12 @@ return {
         vim.cmd('hi DiffDelete guibg=#282828 ctermbg=235 guifg=#fb4934 ctermfg=167 cterm=NONE gui=NONE')
     end,
     keys = {
-        { "<leader>ga", ":Git commit --amend --reset-author<cr>" },
-        { "<leader>gc", ":Git commit<cr>" },
-        { "<leader>gd", ":Git diff<cr>" },
+        { "<leader>ga", ":Git commit --amend --reset-author<cr>", { silent = true } },
+        { "<leader>gc", ":Git commit<cr>", { silent = true } },
+        { "<leader>gd", ":Git diff<cr>", { silent = true } },
         { "<leader>gg", ":Git push<cr>", { silent = true } },
         { "<leader>gp", ":Git pull<cr>", { silent = true } },
-        { "<leader>gs", ":Git<cr>" },
+        { "<leader>gs", ":Git<cr>", { silent = true } },
         { "<leader>gw", ":Gwrite<cr>", { silent = true } },
     },
 }
