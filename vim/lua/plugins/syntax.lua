af40683aeaee591f2ced7c282b3b992f323ca172
Chin Leung <hello@chinleung.com>
2024-11-27 10:01:15 -0500

    Added support for blade

--- vim/lua/plugins/syntax.lua
+++ vim/lua/plugins/syntax.lua
@@ -2,9 +2,19 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     config = function ()
+        require('nvim-treesitter.parsers').get_parser_configs().blade = {
+            install_info = {
+                url = 'https://github.com/EmranMR/tree-sitter-blade',
+                files = { 'src/parser.c' },
+                branch = 'main',
+            },
+            filetype = 'blade',
+        }
+
         require('nvim-treesitter.configs').setup{
             ensure_installed = {
                 'bash',
+                'blade',
                 'html',
                 'javascript',
                 'markdown',
