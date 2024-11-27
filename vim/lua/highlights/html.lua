facd4d6a6e7f538ebab7ebebba1a33cd61f8d40d
Chin Leung <hello@chinleung.com>
2024-11-27 11:37:16 -0500

    Updated highlight for urls

--- vim/lua/highlights/html.lua
+++ vim/lua/highlights/html.lua
@@ -2,6 +2,6 @@ vim.api.nvim_set_hl(0, '@constant.html', { link = 'GruvboxGray' })
 vim.api.nvim_set_hl(0, '@doctype.delimiter.html', { link = 'GruvboxGray' })
 vim.api.nvim_set_hl(0, '@markup.heading.html', { link = 'GruvboxWhite' })
 vim.api.nvim_set_hl(0, '@string.html', { link = 'GruvboxGreen' })
+vim.api.nvim_set_hl(0, '@string.special.url.html', { link = 'GruvboxGreen' })
 vim.api.nvim_set_hl(0, '@tag.delimiter.html', { link = 'GruvboxWhite' })
 vim.api.nvim_set_hl(0, '@tag.html', { link = 'GruvboxAqua' })
-
