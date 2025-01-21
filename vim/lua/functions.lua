5ee356bcc5ebe578b49d6d3024b3ffe44ff332dc
Chin Leung <hello@chinleung.com>
2025-01-21 08:46:28 -0500

    Start with TreeSitter folds off

--- vim/lua/functions.lua
+++ vim/lua/functions.lua
@@ -194,7 +194,7 @@ function generate_uuid()
 end
 
 -- Toggle the folds with TreeSitter
-vim.g.fold_with_treesitter = 1
+vim.g.fold_with_treesitter = 0
 function toggle_folds()
     local current = vim.g.fold_with_treesitter
 
