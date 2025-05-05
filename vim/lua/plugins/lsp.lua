0dc4ae4a18c3d04f1c5f4da2d572c35ec8a9ac22
Chin Leung <hello@chinleung.com>
2025-05-05 12:06:29 -0400

    Re-activate the dependencies

--- vim/lua/plugins/lsp.lua
+++ vim/lua/plugins/lsp.lua
@@ -1,8 +1,8 @@
 return {
     'neovim/nvim-lspconfig',
-    -- dependencies = {
-    --    'williamboman/mason-lspconfig.nvim',
-    -- },
+    dependencies = {
+       'williamboman/mason-lspconfig.nvim',
+    },
     config = function ()
         local lsp = require('lspconfig')
 
