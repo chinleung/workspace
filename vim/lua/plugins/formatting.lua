8e862c59aef1bdb2c2b678e69969f5eb8213d414
Chin Leung <hello@chinleung.com>
2025-05-26 13:14:21 -0400

    chore: adjusted configuration of PHP CS Fixer

--- vim/lua/plugins/formatting.lua
+++ vim/lua/plugins/formatting.lua
@@ -5,8 +5,17 @@ return {
             timeout_ms = 500,
             lsp_format = 'fallback',
         },
+        formatters = {
+            php_cs_fixer = {
+                args = {
+                    'fix',
+                    '$FILENAME',
+                    '--config='..vim.fn.expand('~/workspace/php/.php-cs-fixer.php')
+                },
+            },
+        },
         formatters_by_ft = {
-            php = { 'pint', 'php_cs_fixer' },
+            php = { 'pint', 'php_cs_fixer', stop_after_first = true },
             vue = { 'prettier' },
         },
     },
