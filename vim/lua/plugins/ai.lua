c99d64ad73e18e4fbb4041fae94d3bf6f0571ece
Chin Leung <hello@chinleung.com>
2025-08-14 09:05:11 -0400

    Set up for Laravel Boost

--- vim/lua/plugins/ai.lua
+++ vim/lua/plugins/ai.lua
@@ -10,8 +10,23 @@ return {
     dependencies = {
         'nvim-lua/plenary.nvim',
         'nvim-treesitter/nvim-treesitter',
+        'ravitemer/mcphub.nvim',
     },
     opts = {
+        extensions = {
+            mcphub = {
+                callback = 'mcphub.extensions.codecompanion',
+                opts = {
+                    make_tools = true,
+                    show_server_tools_in_chat = true,
+                    add_mcp_prefix_to_tool_names = false,
+                    show_result_in_chat = true,
+                    format_tool = nil,
+                    make_vars = true,
+                    make_slash_commands = true,
+                },
+            },
+        },
         strategies = {
             chat = {
                 adapter = 'copilot',
