a603cf941a8a1a9ea9413e6a57b8b6c404df5700
Chin Leung <hello@chinleung.com>
2025-10-20 08:37:41 -0400

    feat: set color scheme of lazygit to Gruvbox

---
+++ vim/lua/plugins/git.lua
@@ -0,0 +1,47 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    lazygit = {
+      theme = {
+        activeBorderColor = {
+          fg = "GruvboxYellow",
+          bold = true,
+        },
+        cherryPickedCommitBgColor = {
+          fg = "GruvboxGreen",
+        },
+        defaultFgColor = {
+          fg = "GruvboxFg1",
+        },
+        defaultTextColor = {
+          fg = "GruvboxFg1",
+        },
+        inactiveBorderColor = {
+          fg = "GruvboxGray",
+        },
+        optionsTextColor = {
+          fg = "GruvboxAqua",
+        },
+        selectedLineBgColor = {
+          fg = "GruvboxBg1",
+        },
+        squashCommitBgColor = {
+          fg = "GruvboxPurple",
+        },
+        stagedChangesColor = {
+          fg = "GruvboxGreen",
+        },
+        unstagedChangesColor = {
+          fg = "GruvboxRed",
+        },
+      },
+    },
+    styles = {
+      lazygit = {
+        wo = {
+          winhighlight = "Normal:Normal",
+        },
+      },
+    },
+  },
+}
