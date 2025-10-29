30fdcca33037ede0d0b3b997de66b25682c0717b
Chin Leung <hello@chinleung.com>
2025-10-29 12:20:00 -0400

    chore: updated keymaps registration
    Use the new structure for setting keymaps.

--- vim/lua/plugins/lsp.lua
+++ vim/lua/plugins/lsp.lua
@@ -1,9 +1,19 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = function()
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-
-    keys[#keys + 1] = { "K", false }
-    keys[#keys + 1] = { "D", function() return vim.lsp.buf.hover() end, desc = 'Hover' }
-  end,
+  opts = {
+    servers = {
+      ["*"] = {
+        keys = {
+          { "K", false },
+          {
+            "D",
+            function()
+              return vim.lsp.buf.hover()
+            end,
+            desc = "Hover",
+          },
+        },
+      },
+    },
+  },
 }
